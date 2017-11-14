fe6ba023853b0b79894fa159cd3b8bd49cdc9339
Ajordy <32485526+Ajordy@users.noreply.github.com>
2017-11-14 21:44:02 +0700

    Add files via upload

---
+++ IF11_10116497_LATIHAN4/src/IF11_10116497_LATIHAN4/ModelProgrammer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IF11_10116497_LATIHAN4;
+
+/**
+ *
+ * @author Adam jordy
+ */
+public class ModelProgrammer extends ModelEmployee {
+    
+    int bonus = 60000;
+    
+    public void bonusProgrammer(){
+
+        System.out.println("Salary Employee Designer : " + salary);
+        System.out.println("Bonus Programmer : " + bonus);    
+    }
+    
+}
---
+++ IF11_10116497_LATIHAN4/src/IF11_10116497_LATIHAN4/ModelSystemAnalyst.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IF11_10116497_LATIHAN4;
+
+/**
+ *
+ * @author Adam jordy
+ */
+public class ModelSystemAnalyst extends ModelEmployee {
+
+    int bonus = 90000;
+
+    public void bonusSystemAnalyst() {
+        System.out.println("Salary Employee Designer : " + salary);
+        System.out.println("Bonus SystemAnalyst : " + bonus);
+    }
+}
